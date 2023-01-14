c55c7b71f408a334a6edc913e049a23ad1fc2d6d
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-13 19:49:51 -0500

    main function

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var totalRain = 404;
 function randRange( minNum, maxNum) {
   return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
 }
+
+function createRain() {
+	for( i=1; i < totalRain; i++ ) {
+    var dropLeft = randRange(0,1600);
+    var dropTop = randRange(-1000,1400);
+    $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+    $('#drop'+i).css('left',dropLeft);
+    $('#drop'+i).css('top',dropTop);
+	}
+}
